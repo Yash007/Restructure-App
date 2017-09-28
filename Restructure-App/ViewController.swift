2913b12e7302b186d3abb196fcb7840e6d3fff4a
YASH SOMPURA <yash007@YASHs-MacBook-Pro.local>
2017-09-27 20:53:25 -0400

    Initial Commit

---
+++ Restructure-App/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Restructure-App
+//
+//  Created by YASH SOMPURA on 27/09/17.
+//  Copyright © 2017 YASH SOMPURA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
